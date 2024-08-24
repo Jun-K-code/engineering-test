df987fa32f0a6b56f2b257861a6882212a61f47b
Jun-K-code <JunCode@aliyun.com>
2024-08-24 20:38:35 +0800

    fix(commitlint): 进行commitlint提交测试

--- apps/ex-app/src/index.ts
+++ apps/ex-app/src/index.ts
@@ -1,9 +1,9 @@
 // import { add } from '@startup-monorepo-fastify-phaser-colyseus-threejs/utils';
 
-// const main = () => {
-//   console.log('测试', add(1, 2));
-// };
-// main();
+const main = () => {
+  // console.log('测试', add(1, 2));
+};
+main();
 
 // function test() {
 //   console.log('test');
